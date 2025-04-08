2d863224c32a090a769dc91f55355e4c9fbdf594
ajimeno04 <jimenobeltran11@gmail.com>
2025-04-08 09:50:12 +0200

    refactor(SidebarFooter): enhance footer with new suggestion link and help center access

--- apps/web/src/components/sidebar/SidebarFooter/index.tsx
+++ apps/web/src/components/sidebar/SidebarFooter/index.tsx
@@ -1,34 +1,39 @@
 import type { ReactElement } from 'react'
 import { useEffect } from 'react'
 
-import { SidebarList } from '@/components/sidebar/SidebarList'
+import {
+  SidebarList,
+  SidebarListItemButton,
+  SidebarListItemIcon,
+  SidebarListItemText,
+} from '@/components/sidebar/SidebarList'
 import { loadBeamer } from '@/services/beamer'
-import { useAppDispatch, useAppSelector } from '@/store'
+import { useAppSelector } from '@/store'
 import { CookieAndTermType, hasConsentFor } from '@/store/cookiesAndTermsSlice'
-import { openCookieBanner } from '@/store/popupSlice'
-// import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
-import { ListItem } from '@mui/material'
+import { ListItem, SvgIcon, Typography } from '@mui/material'
 import DebugToggle from '../DebugToggle'
-import { IS_PRODUCTION } from '@/config/constants'
+import { IS_PRODUCTION, HELP_CENTER_URL } from '@/config/constants'
 import { useCurrentChain } from '@/hooks/useChains'
+import ProtofireLogo from '@/public/images/protofire-logo.svg'
+import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
+import SuggestionIcon from '@/public/images/sidebar/lightbulb_icon.svg'
+import ExternalLink from '@/components/common/ExternalLink'
+import Track from '@/components/common/Track'
+import { OVERVIEW_EVENTS } from '@/services/analytics'
+
+export const NEW_SUGGESTION_FORM =
+  'https://docs.google.com/forms/d/e/1FAIpQLSfojsADYCiWq9AqbLqsUTzCDSpA8FMgdAQp0Pyl0BOeurlq9A/viewform'
 
 const SidebarFooter = (): ReactElement => {
-  const dispatch = useAppDispatch()
   const chain = useCurrentChain()
   const hasBeamerConsent = useAppSelector((state) => hasConsentFor(state, CookieAndTermType.UPDATES))
 
   useEffect(() => {
-    // Initialise Beamer when consent was previously given
+    // Initialize Beamer when consent was previously given
     if (hasBeamerConsent && chain?.shortName) {
       loadBeamer(chain.shortName)
     }
   }, [hasBeamerConsent, chain?.shortName])
-
-  const _handleBeamer = () => {
-    if (!hasBeamerConsent) {
-      dispatch(openCookieBanner({ warningKey: CookieAndTermType.UPDATES }))
-    }
-  }
 
   return (
     <SidebarList>
@@ -40,29 +45,61 @@ const SidebarFooter = (): ReactElement => {
 
       {/* <Track {...OVERVIEW_EVENTS.WHATS_NEW}>
         <ListItem disablePadding>
-          <SidebarListItemButton id={BEAMER_SELECTOR} onClick={handleBeamer}>
+          <SidebarListItemButton id={BEAMER_SELECTOR}>
             <SidebarListItemIcon color="primary">
-              <BeamerIcon />
+              <HelpCenterIcon />
             </SidebarListItemIcon>
-            <SidebarListItemText data-testid="list-item-whats-new" bold>
-              What&apos;s new
+            <SidebarListItemText data-testid="list-item-need-help" bold>
+              What&apos;s New
             </SidebarListItemText>
           </SidebarListItemButton>
         </ListItem>
       </Track> */}
 
-      {/* <ListItem disablePadding>
-        <a target="_blank" rel="noopener noreferrer" href={HELP_CENTER_URL} style={{ width: '100%' }}>
-          <SidebarListItemButton>
-            <SidebarListItemIcon color="primary">
-              <HelpCenterIcon />
-            </SidebarListItemIcon>
-            <SidebarListItemText data-testid="list-item-need-help" bold>
-              Need help?
-            </SidebarListItemText>
-          </SidebarListItemButton>
-        </a>
-      </ListItem> */}
+      {/* Help Center link removed. To restore, import HELP_CENTER_URL from '@/config/constants' */}
+      <Track {...OVERVIEW_EVENTS.HELP_CENTER}>
+        <ListItem disablePadding>
+          <a target="_blank" rel="noopener noreferrer" href={HELP_CENTER_URL} style={{ width: '100%' }}>
+            <SidebarListItemButton>
+              <SidebarListItemIcon color="primary">
+                <HelpCenterIcon />
+              </SidebarListItemIcon>
+              <SidebarListItemText data-testid="list-item-need-help" bold>
+                Need help?
+              </SidebarListItemText>
+            </SidebarListItemButton>
+          </a>
+        </ListItem>
+      </Track>
+      <Track {...OVERVIEW_EVENTS.SUGGESTIONS}>
+        <ListItem disablePadding>
+          <a target="_blank" rel="noopener noreferrer" href={NEW_SUGGESTION_FORM} style={{ width: '100%' }}>
+            <SidebarListItemButton style={{ backgroundColor: '#5FDDFF', color: 'black' }}>
+              <SidebarListItemIcon color="primary">
+                <SuggestionIcon />
+              </SidebarListItemIcon>
+              <SidebarListItemText bold>New Features Suggestion?</SidebarListItemText>
+            </SidebarListItemButton>
+          </a>
+        </ListItem>
+      </Track>
+
+      <ListItem>
+        <SidebarListItemText>
+          <Typography variant="caption">
+            Supported by{' '}
+            <SvgIcon
+              component={ProtofireLogo}
+              inheritViewBox
+              fontSize="small"
+              sx={{ verticalAlign: 'middle', mx: 0.5 }}
+            />
+            <ExternalLink href="https://protofire.io" sx={{ textDecoration: 'none' }} noIcon>
+              Protofire
+            </ExternalLink>
+          </Typography>
+        </SidebarListItemText>
+      </ListItem>
     </SidebarList>
   )
 }
