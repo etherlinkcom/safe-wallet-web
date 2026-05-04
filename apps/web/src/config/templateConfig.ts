0f718552c6d4c37bf041897f44f8fae90f49c3d1
DenSmolonski <denissmolonski@gmail.com>
2026-05-04 13:57:16 +0200

    feat: update defautl template chain id

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,18 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/etherlink/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  SAFE_UTILS_SUPPORTED: false,
+  IS_LICENSED: false,
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '24px',
+    },
+  },
+} as TemplateConfig
+export default TEMPLATE_CONFIG
